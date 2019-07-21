ca837303df0fdc991ef41f48366d1f76b2b5f497
WUPA2 <panda.wu@oocl.com>
2019-07-21 18:28:06 +0800

    finish show todo list

---
+++ todo-list/src/components/TodoList.vue
@@ -0,0 +1,22 @@
+<template>
+    <div class="todo-list-container">
+        <p>Todo List</p>
+        <div><i>Simple Todo List with adding and filter by diff status.</i></div>
+        <input type="text"><Button>Add</Button>
+        <div class="item-list-container">
+            <div class="item-list" v-for="(item,index) in itemList" :key="index">
+                {{index}}.<checkbox></checkbox>{{item}}
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    data() { 
+        return {
+            itemList:[123,456]
+        }
+    }
+ }
+</script>
