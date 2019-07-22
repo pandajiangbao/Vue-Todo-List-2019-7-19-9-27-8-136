8ffcf79680d55033a9bca0a0fef2b212c0a50652
WUPA2 <panda.wu@oocl.com>
2019-07-22 10:52:56 +0800

    refactor components

---
+++ todo-list/src/components/TodoItem.vue
@@ -0,0 +1,43 @@
+<template>
+    <div class="item-list">
+        <span v-if="(item.isSelected==false)&&(type=='All'||type=='Active')">
+            {{index+1}}.
+            <span class="color-zone" @dblclick="item.isEdited=true">
+                <input
+                    type="text"
+                    v-if="item.isEdited"
+                    v-model="item.value"
+                    @blur="item.isEdited=false"
+                />
+                <span v-if="!item.isEdited">
+                    <input type="checkbox" v-model="item.isSelected" />
+                    {{item.value}}
+                </span>
+            </span>
+        </span>
+        <del v-if="(item.isSelected==true)&&(type=='All'||type=='Complete')">
+            {{index+1}}.
+            <span class="color-zone" @dblclick="item.isEdited=true">
+                <input
+                    type="text"
+                    v-if="item.isEdited"
+                    v-model="item.value"
+                    @blur="item.isEdited=false"
+                />
+                <span v-if="!item.isEdited">
+                    <input type="checkbox" v-model="item.isSelected" />
+                    {{item.value}}
+                </span>
+            </span>
+        </del>
+    </div>
+</template>
+
+<script>
+export default {
+    props:["index","item","type"]
+};
+</script>
+
+<style>
+</style>
--- todo-list/src/components/TodoList.vue
+++ todo-list/src/components/TodoList.vue
@@ -1,43 +1,49 @@
 <template>
-  <div class="todo-list-container">
-    <p>Todo List</p>
-    <div>
-      <i>Simple Todo List with adding and filter by diff isSelected.</i>
+    <div class="todo-list-container">
+        <p>Todo List</p>
+        <div>
+            <i>Simple Todo List with adding and filter by diff isSelected.</i>
+        </div>
+        <div class="item-list-input-container">
+            <input v-model="content.value" type="text" @keyup.enter="addItem" />
+            <Button @click="addItem">Add</Button>
+        </div>
+        <div class="item-list-container">
+            <todo-item v-for="(item,index) in itemList" :key="index" :index="index" :item="item" :type="type"/>
+        </div>
+        <div class="item-list-button-container">
+            <button @click="type='All'">All</button>
+            <button @click="type='Active'">Active</button>
+            <button @click="type='Complete'">Complete</button>
+        </div>
     </div>
-    <div class="item-list-input-container">
-      <input v-model="content.value" type="text" />
-      <Button @click="addItem">Add</Button>
-    </div>
-    <div class="item-list-container">
-      <div class="item-list" v-for="(item,index) in itemList" :key="index">
-        <span v-if="(item.isSelected==false)&&(clickType=='All'||clickType=='Active')">{{index+1}}.<span class="color-zone"><input type="checkbox" v-model="item.isSelected" />{{item.value}}</span></span>
-        <del v-if="(item.isSelected==true)&&(clickType=='All'||clickType=='Complete')">{{index+1}}.<span class="color-zone"><input type="checkbox" v-model="item.isSelected" />{{item.value}}</span></del>
-      </div>
-    </div>
-    <div class="item-list-button-container">
-      <button @click="clickType='All'">All</button>
-      <button @click="clickType='Active'">Active</button>
-      <button @click="clickType='Complete'">Complete</button>
-    </div>
-  </div>
 </template>
 
 <script>
+import TodoItem from './TodoItem.vue'
+
 export default {
-    data() { 
-        return {
-            content:{value:'',isSelected:false},
-            itemList:[],
-            clickType:'All'
-        }
+    components:{
+        TodoItem
     },
-    methods:{
-        addItem(){
-            if(this.content.value!=''){
-                this.itemList.push(this.content)
-                this.content={value:'',isSelected:false}
+    data() {
+        return {
+            content: { value: "", isSelected: false, isEdited: false },
+            itemList: [],
+            type: "All"
+        };
+    },
+    methods: {
+        addItem() {
+            if (this.content.value != "") {
+                this.itemList.push(this.content);
+                this.content = {
+                    value: "",
+                    isSelected: false,
+                    isEdited: false
+                };
             }
         }
     }
- }
+};
 </script>
